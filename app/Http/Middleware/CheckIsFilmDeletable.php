87b93564a524031d6167722567d600ad2a6c8f27
BrendonMorel <BrendonMorel@users.noreply.github.com>
2024-04-29 09:23:54 -0400

    Correction du middleware CheckIsFilmDeletable

--- app/Http/Middleware/CheckIsFilmDeletable.php
+++ app/Http/Middleware/CheckIsFilmDeletable.php
@@ -13,17 +13,18 @@ class CheckIsFilmDeletable
     {
         $film_id = $request->route('id');
 
-        // Vérifier s'il existe une critique pour ce film
-        $existingCritic = Critic::where('film_id', $film_id)->exists();
-
-        // Vérifier s'il existe une relation pour ce film et un acteur
         $film = Film::find($film_id);
         if (!$film) {
             return $next($request);
         }
         
+        // Vérifier s'il existe une critique pour ce film
+        $existingCriticRelation = $film->critics()->exists();
+
+        // Vérifier s'il existe une relation pour ce film et un acteur
         $existingActorRelation = $film->actors()->exists();
-        if ($existingCritic || $existingActorRelation) {
+        
+        if ($existingCriticRelation || $existingActorRelation) {
             abort(FORBIDDEN, 'Forbidden');
         }
 
